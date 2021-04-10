64c74c43d4cfef74a66919122ab801021cef2c79
Olga Matsiuk <matsiuk2015@gmail.com>
2021-04-10 23:43:39 +0300

    испавления в 1 и 6 задаче

--- js/task-01.js
+++ js/task-01.js
@@ -1,30 +1,9 @@
-const categories = document.querySelectorAll('.item').length;
-console.log(`В списке ${categories} категории`);
+const itemsRef = document.querySelectorAll('.item');
+console.log(`В списке ${itemsRef.length} категории.`);
 
-const animalsTitle = document.querySelector('.animalsTitle').textContent;
-console.log(`Категория: ${animalsTitle}`);
-const animals = document.querySelectorAll('.animals').length;
-console.log(`Количество элементов: ${animals}`);
-
-const productsTitle = document.querySelector('.productsTitle').textContent;
-console.log(`Категория: ${productsTitle}`);
-const products = document.querySelectorAll('.products').length;
-console.log(`Количество элементов: ${products}`);
-
-const technologiesTitle = document.querySelector('.technologiesTitle').textContent;
-console.log(`Категория: ${technologiesTitle}`);
-const technologies = document.querySelectorAll('.technologies').length;
-console.log(`Количество элементов: ${technologies}`);
-// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-//     let message;
-//     // Пиши код ниже этой строки
-//   const totalPrice = pricePerDroid * orderedQuantity;
-//     if customerCredits>totalPrice {
-//       message = `Вы купили ${orderedQuantity} дроидов, на счету осталось ${customerCredits-totalPrice} кредитов`; }
-//       else {
-//         message = 'Недостаточно средств на счету!';
-//       }
-//     // Пиши код выше этой строки
-//     return message;
-//   }
-//   console.log (makeTransaction(3000, 5, 23000));
+itemsRef.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const elementsQuantity = item.querySelectorAll('li').length;
+    console.log(`Категория: ${title}`)
+    console.log(`Количество элементов: ${elementsQuantity}`)
+})
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,15 @@
 const input= document.querySelector('#validation-input');
 input.addEventListener('change',onInputChange);
+input.addEventListener('focus', onInputFocus);
+const symbolsQuantity = Number(input.getAttribute('data-length'));
+
 function onInputChange (event){
-   if (event.currentTarget.value.length===6){
+   if (event.currentTarget.value.length===symbolsQuantity){
     input.classList.add("valid");
 }
     else input.classList.add("invalid");
     
+}
+function onInputFocus() {
+    input.classList.remove('valid', 'invalid');
 }
